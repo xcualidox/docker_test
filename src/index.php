ed132b470f69bd20153d9e9faac03b2705247f75
Alex Cruz <xcualidoxkuruzu@gmail.com>
2023-04-24 20:16:04 -0400

    add html

--- src/index.php
+++ src/index.php
@@ -1,3 +1,22 @@
-<?php
-echo "hola mundo";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <h1>Esto es una prueba</h1>
+    <h2>Lista de numeros</h2>
+    <ul>
+
+    
+    <?php
+        for($i=0;$i<10;$i++){
+            echo "<li>Numero $i</li>";
+        }
+    ?>
+    </ul>
+</body>
+</html>
